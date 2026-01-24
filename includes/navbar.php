37b5d151b2e2c41a4243b5e6e8dcce31b7dfd824
razvantaranu03 <razvan.taranu03@e-uvt.ro>
2026-01-24 02:18:24 +0200

    Add 'Istorie' link to the navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -8,6 +8,9 @@
 
         <div class="collapse navbar-collapse" id="mainNavbar">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                 <li class="nav-item">
+                    <a class="nav-link" href="istorie.php">Istorie</a>
+                </li>
                 <li class="nav-item">
                     <a class="nav-link" href="transport.php">Transport & Cazare</a>
                 </li>
